ed073f8d6aac0453f13a80289224cf30677b8d32
João Thallis <joaothallis@icloud.com>
2023-02-05 16:33:46 -0300

    Remove debug `abbr`

--- start_streaming.fish
+++ start_streaming.fish
@@ -4,7 +4,6 @@ builtin history clear
 abbr --add nvim "nvim -i NONE"
 abbr --add env "echo 'env command is disabled'"
 abbr --add export "echo 'export command is disabled'"
-abbr --add x "echo 'export command is disabled'"
 
 google-chrome-stable &
 
