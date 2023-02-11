aafccafcb5d49abb5fc4b41bcec826179d9ddc13
João Thallis <joaothallis@icloud.com>
2023-02-11 16:42:34 -0300

    Support MacOS

--- start_streaming.fish
+++ start_streaming.fish
@@ -5,8 +5,16 @@ abbr --add nvim "nvim -i NONE"
 abbr --add env "echo 'env command is disabled'"
 abbr --add export "echo 'export command is disabled'"
 
-google-chrome-stable &
+switch (uname -a)
+    case 'Darwin*'
+        open -a "Google Chrome"
 
-if not pgrep -x "obs" > /dev/null
-    obs --startstreaming
+        open -a "OBS"
+    case '*'
+        google-chrome-stable &
+
+        if not pgrep -x "obs" > /dev/null
+            obs --startstreaming
+        end
 end
+
